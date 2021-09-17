edfa5f60dabeeb3173891c648c05a54393975441
Oscar Mendoza <oscar.mendozaruiz@hotmail.com>
2021-09-17 11:51:11 -0500

    versionamiento de ajustes de persistencia

--- target/generated-sources/jaxb/com/gdr/gs_ws/IdPersona.java
+++ target/generated-sources/jaxb/com/gdr/gs_ws/IdPersona.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2021.09.13 a las 05:01:27 PM COT 
+// Generado el: 2021.09.16 a las 10:39:19 PM COT 
 //
 
 
